c5198d6e5fbe5e1838d66df60954f1519f72dead
Christopher Thrasher <cthrasher@nevada.unr.edu>
2024-05-08 06:14:25 +0000

    images

--- images.c
+++ images.c
@@ -1,209 +1,197 @@
-//Names: Christopher Thrasher and Noah Ramos
-//Date: 4/29/24
-//Assignment: Final Group Project
+//final Project 
+//Authors: Christopher Thrasher & Noah Ramos
 
 #include <stdio.h>
-
-#define MAXWIDTH 100
-#define MAXHEIGHT 100
+#define ROWS 100
+#define COLUMNS 100
+#define FILENAME_LENGTH 25
 FILE *filePtr1 ;
-
-void loadImage(int rows, int columns, int loadArr[][columns]);
-
-void displayImage(int rows, int columns, int dispArr[][columns]);
-
-void brightenImage(int rows, int columns, int brightArr[][columns]);
-
-void dimImage(int rows, int columns, int dimArr[][columns]);
-
-void saveImage(int rows, int columns, int saveArr[][columns]);
-
-void cropImage(int rows, int columns, int cropArr[][columns]);
-
-void editMenu(int rows, int columns, int editArr[][columns]);
 
 void mainMenu();
 
-int main() {
-    int pixels[MAXWIDTH][MAXHEIGHT];
-    
-    int choice;
+void loadImage(int rows, int columns, char loadArr[][columns], int* maxRow, int* maxColumn);
 
-    do {
-      printf("**ERINSTAGRAM**\n");
-      printf("1: Load image\n");
-      printf("2: Display image\n");
-      printf("3: Edit image\n");
-      printf("0: Exit\n");
-      printf("\nChoose from one of the options above: ");
-      scanf("%d", &choice);
+void displayImage(int rows, int columns, char dispArr[][columns]);
 
-	switch (choice) {
-	case 1:
-		{
-		loadImage(MAXHEIGHT, MAXWIDTH, pixels);
-		printf("Image successfully loaded!\n\n");
+void editImage();
+
+void dimImage(int rows, int columns, char dimArr[][columns]);
+
+void brightImage(int rows, int columns, char brightArr[][columns]);
+
+void save(int rows, int columns, char saveArr[][columns]);
+
+void cropImage(int rows, int columns, char cropArr[][columns]);
+
+int main(){
+
+	mainMenu();	
+	
+}
+
+void mainMenu(){
+	int menuChoice;
+	int imageRows, imageColumns;
+	char savedImage[ROWS][COLUMNS];
+	do{
+	printf("**IMAGE EDITOR**\n1: Load image\n2: Display Image\n3: Edit Image\n0: Exit\n\nChoose from one of the above:");
+	scanf("%d", &menuChoice);
+	switch(menuChoice){
+		case 1:loadImage(ROWS, COLUMNS, savedImage, &imageRows, &imageColumns);
+			printf("the rows are %d, and columns are %d\n", imageRows, imageColumns);
+			int indexRow=0, indexColumn=0;
+			
 			break;
+		case 2:displayImage(imageRows, imageColumns, savedImage);
+		for(indexRow;indexRow<imageRows+1;indexRow++){
+			for(indexColumn;indexColumn<imageColumns+1;indexColumn++){
+			switch("c", savedImage[indexRow][indexColumn]){
+			case'0':
+				printf(" ");
+				break;
+			case'1':
+				printf(".");
+				break;
+			case'2':
+				printf("o");
+				break;
+			case'3':
+				printf("O");
+				break;
+			case'4':
+				printf("0");
+				break;
+			}
+			}
+			indexColumn = 0;
+			printf("\n");
 		}
-	case 2:
-		displayImage(MAXHEIGHT, MAXWIDTH, pixels);
-		printf("\n");
 			break;
-	case 3:{
-		editMenu(MAXHEIGHT, MAXWIDTH, pixels);
+		case 3:editImage(imageRows, imageColumns, savedImage);
+			break;
+		case 0:printf("\nGoodbye!\n");
+			break;
 	}
-	case 0:
-	printf("Goodbye!\n");
-	break;
-	default:
-	printf("Invalid option. Please try again.\n\n");
-	break;
-	}
-	} while (choice != 0);
-
-    return 0;
+	}while(menuChoice != 0);
 }
-
-void editMenu(int rows, int columns, int editArr[][columns]){
-	int width = 0, height = 0, X, Y;
-            if (width > 0 && height > 0) {
-                int editChoice;
-                do {
-                printf("**EDITING**\n");
-                printf("1: Crop image\n");
-                printf("2: Dim image\n");
-                printf("3: Brighten image\n");
-                printf("0: Return to main menu\n");
-                printf("\nChoose from one of the options above: ");
-                scanf("%d", &editChoice);
-
-     switch (editChoice) {
-     case 1:
-     {
-     int startA, startB, newWidth, newHeight;
-       printf("The image you want to crop is %d x %d.\n", width, height);
-       printf("The row and column values start in the upper lefthand corner.\n\n");
-       printf("Which column do you want to be the new left side? ");
-       scanf("%d", &X);
-       printf("\nWhich column do you want to be the new right side? ");
-       scanf("%d", &newWidth);
-       printf("\nWhich row do you want to be the new top? ");
-       scanf("%d", &Y);
-       printf("\nWhich row do you want to be the new bottom? ");
-       scanf("%d", &newHeight);
-
-       cropImage(MAXHEIGHT, MAXWIDTH, editArr);
-       printf("\n");
-
-       char saveChoice;
-       printf("Would you like to save the file? (y/n) ");
-       scanf(" %c", &saveChoice);
-         if (saveChoice == 'y' || saveChoice == 'Y') {
-         char filename[100];
-         printf("\nWhat do you want to name the image file? (include the extension) ");
-         scanf("%s", filename);
-         saveImage(MAXHEIGHT, MAXWIDTH, editArr);
-         printf("Image successfully saved!\n\n");
-         }
-         break;
-         }
-         case 2:
-         {
-         dimImage(MAXHEIGHT, MAXWIDTH, editArr);
-                                        
-         char saveChoice;
-         printf("Would you like to save the file? (y/n) ");
-         scanf(" %c", &saveChoice);
-            if (saveChoice == 'y' || saveChoice == 'Y') {
-            char filename[100];
-            printf("\nWhat do you want to name the image file? (include the extension) ");
-            scanf("%s", filename);
-            saveImage(MAXHEIGHT, MAXWIDTH, editArr);
-            printf("Image successfully saved!\n\n");
-            }
-            break;
-            }
-            case 3:
-            {
-            brightenImage(MAXHEIGHT, MAXWIDTH, editArr);
-                                                                               
-            char saveChoice;
-            printf("Would you like to save the file? (y/n) ");
-            scanf(" %c", &saveChoice);
-               if (saveChoice == 'y' || saveChoice == 'Y') {
-               char filename[100];
-               printf("\nWhat do you want to name the image file? (include the extension) ");
-               scanf("%s", filename);
-               saveImage(MAXHEIGHT, MAXWIDTH, editArr);
-               printf("Image successfully saved!\n\n");
-              }
-               break;
-               }
-          case 0:
-          break;
-          default:
-          printf("Invalid option. Please try again.\n\n");
-          break;
-          }
-         } 
-         while (editChoice != 0);
-         }
-         else {
-         printf("Sorry, no image to edit\n\n");
-         }
-}
-
-void loadImage(int rows, int columns, int loadArr[][columns]){
-	int index1, index2, tempVal;
-	char filename[100];
-		printf("What is the name of the image file? ");
-		scanf("%s", filename);
-	printf("the file you want to acess is %s \n", filename);
-	filePtr1= fopen (filename,"r");
+void loadImage(int rows, int columns, char loadArr[][columns], int* maxRow, int* maxColumn){
+	int indexRow=0, indexColumn=0;
+	char tempVal;
+	char fileName[FILENAME_LENGTH+1];
+	printf("this is where the image will be loaded\nWhat is the name of the image file? ");
+	scanf("%s", fileName);
+	filePtr1= fopen (fileName,"r");
 		if(filePtr1 == NULL){
 			printf("cannot open file\n");
-			return;
 		}
-		printf("Image suessfully loaded!\n");
-		
-		
-		for(index1 = 0;index1<rows;index1++){
-			for(index2 = 0;index2<columns;index2++){
-				}while(loadArr[index1][index2]!= EOF);
-					}
-				  if (fscanf(filePtr1, "%d", &tempVal) != 1){
-                                    printf("Could not find an image with that filename.\n");
-				
-				    fclose(filePtr1);
-                                    return;
-                                    loadArr[index1][index2] = tempVal; 
-					}
+		else{
+		while(fscanf(filePtr1, "%c", &tempVal)==1)
+			
+			if(tempVal=='\n'){
+			indexRow++;
+			*maxColumn = indexColumn;
+			indexColumn = 0;
 			}
-			fclose(filePtr1); 
+			else{
+			loadArr[indexRow][indexColumn]=tempVal;
+			indexColumn++;
+			}
+		}
+		 *maxRow = indexRow;
+	}
+void displayImage(int rows, int columns, char dispArr[][columns]){
+	int indexRow=0, indexColumn=0;
+	for(indexRow;indexRow<rows+1;indexRow++){
+			for(indexColumn;indexColumn<columns+1;indexColumn++){
+			switch("c", dispArr[indexRow][indexColumn]){
+			case'0':
+				printf(" ");
+				break;
+			case'1':
+				printf(".");
+				break;
+			case'2':
+				printf("o");
+				break;
+			case'3':
+				printf("O");
+				break;
+			case'4':
+				printf("0");
+				break;
+			}
+			}
+			indexColumn = 0;
+			printf("\n");
+		}
+		
 }
+void editImage(int rows, int columns, char editArr[][columns]){
+	int editChoice;
+	printf("**EDIT IMAGE**\n");
+	printf("1: Brighten image\n");
+	printf("2: Dim image\n");
+	printf("3: Crop image\n");
+	printf("0: Exit\n");
+	printf("Choose how you want to edit:");
+	scanf("%d", &editChoice);
+	
+	do{switch(editChoice){
+		case 1:brightImage(rows, columns, editArr);
+			break;
+		case 2:dimImage(rows, columns, editArr);
+			break;
+		case 3:
+			break;
+		}
+	}while(editChoice!= 0);	
+	printf("GoodBye!\n\n");	
+}
+
+void cropImage(int rows, int columns, char cropArr[][columns]) {
+    int startRow, startColumn, cropRows, cropColumns;
+
+    	printf("Enter start row   ");
+    	scanf("%d", &startRow);
+    	printf("Enter starting colum: ");
+    	scanf("%d", &startColumn);
+    	printf("Enter number of rows to crop: ");
+    	scanf("%d", &cropRows);
+    	printf("Enter number of columns to crop: ");
+    	scanf("%d", &cropColumns);
+
+    	if (startRow >= 0 && startRow < rows && startColumn >= 0 && startColumn < columns
+        	&& cropRows > 0 && cropColumns > 0
+       		&& startRow + cropRows <= rows && startColumn + cropColumns <= columns) {
+
+        	rows = cropRows;
+        	columns = cropColumns;
+
+        for (int i = 0; i < cropRows; i++) {
+            	for (int j = 0; j < cropColumns; j++) {
+                cropArr[i][j] = cropArr[startRow + i][startColumn + j];
+            }
+        }
+
+        printf("Image cropped.\n");
+    } else {
+        printf("Invalid dimension\n");
+    }
+}
+
+void dimImage(int rows, int columns, char dimArr[][columns]){
+	
+}
+
+void brightImage(int rows, int columns, char brightArr[][columns]){
+
+}
+
+void save(int rows, int columns, char saveArr[][columns]){
+	char saveChoice;
+	printf("would you like to save the edited image?");
+	scanf(" %c", &saveChoice);
 }
 
 
-void displayImage(int rows, int columns, int dispArr[][columns]){
-	for(index1 = 0;index1<rows;index1++){
-		for(index2 = 0;index2<columns;index2++){
-			if(dispArr[index1][index2]!= '\0')
-			printf("%d", dispArr[index1][index2]);
-					}
-}
 
-void brightenImage(int rows, int columns, int brightArr[][columns]){
-
-}
-
-void dimImage(int rows, int columns, int dimArr[][columns]){
-
-}
-
-void saveImage(int rows, int columns, int saveArr[][columns]){
-
-}
-
-void cropImage(int rows, int columns, int cropArr[][columns]){
-
-}
